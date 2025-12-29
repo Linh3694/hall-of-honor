e07cebf3e346119321f37d716059303c73e38520
Linh3694 <hailinh.n23@gmail.com>
2025-12-29 16:40:59 +0700

    update

--- src/core/config.jsx
+++ src/core/config.jsx
@@ -1,10 +1,9 @@
 // Updated to use Frappe API endpoints
 export const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://admin.sis.wellspring.edu.vn/api/method/erp.api.erp_sis.hall_of_honor";
+  "https://prod.sis.wellspring.edu.vn/api/method/erp.api.erp_sis.hall_of_honor";
 export const UPLOAD_URL =
-  import.meta.env.VITE_UPLOAD_URL ||
-  "https://admin.sis.wellspring.edu.vn/files";
+  import.meta.env.VITE_UPLOAD_URL || "https://prod.sis.wellspring.edu.vn/files";
 export const BASE_URL =
-  import.meta.env.VITE_URL || "https://admin.sis.wellspring.edu.vn";
+  import.meta.env.VITE_URL || "https://prod.sis.wellspring.edu.vn";
 export const CDN_URL = import.meta.env.VITE_CDN;
